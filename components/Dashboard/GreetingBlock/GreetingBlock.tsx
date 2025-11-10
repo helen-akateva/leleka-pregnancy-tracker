b8e4266444184f3c3b3712f3692a1d4a0987663c
Anton Shuvalov <anshuvalov83@gmail.com>
2025-11-10 16:20:35 +0200

    finished working on the styles

--- components/Dashboard/GreetingBlock/GreetingBlock.tsx
+++ components/Dashboard/GreetingBlock/GreetingBlock.tsx
@@ -2,27 +2,10 @@
 
 import { useAuthStore } from "@/lib/store/authStore";
 import css from "./GreetingBlock.module.css";
-// import { useAuth } from "@/hooks/useAuth";
 
 export default function GreetingBlock() {
-  // const { user, isLoading, isError } = useAuth();
   const { user } = useAuthStore();
 
-  // if (isLoading) {
-  //   return <p className={css.loading}>Loading...</p>;
-  // }
-
-  if (isLoading) {
-    return <p className={css.loading}>Loading...</p>;
-  }
-
-  if (isError || !user?.name) {
-    return (
-      <div className={css.block}>
-        <p className={css.title}>Доброго дня!</p>
-      </div>
-    );
-  }
   return (
     <div className={css.block}>
       <h2 className={css.title}>Доброго ранку, {user?.name}!</h2>
